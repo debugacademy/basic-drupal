3f7c2b15e53301d762fe9892f1d1c28bf6979095
Ashraf Abed <ashraf.abed@debugacademy.com>
2019-07-23 08:07:28 -0400

    Added demo_weather block

---
+++ web/modules/custom/demo_weather/src/Plugin/Block/WeatherBlock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\demo_weather\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+/**
+ * Provides a 'WeatherBlock' block.
+ *
+ * @Block(
+ *  id = "weather_block",
+ *  admin_label = @Translation("Weather block"),
+ * )
+ */
+class WeatherBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Logger\LoggerChannelFactoryInterface definition.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * Constructs a new WeatherBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The LoggerChannelFactoryInterface definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    LoggerChannelFactoryInterface $logger_factory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->loggerFactory = $logger_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['#theme'] = 'weather_block';
+     $build['weather_block']['#markup'] = 'Implement WeatherBlock.';
+
+    return $build;
+  }
+
+}
